1a0e1dacdf6c0b4990e4472b5b958cedafb81c83
Catarina Gonçalves <a93259@alunos.uminho.pt>
2022-06-29 02:19:56 +0100

    front page almost

--- Front-End/recipe_app/src/App.js
+++ Front-End/recipe_app/src/App.js
@@ -1,52 +1,42 @@
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FcGoogle } from 'react-icons/fc';
 import React from 'react'
 import './App.css';
 
 function App() {
   return (
     <div>
-      <FontAwesomeIcon icon="coffee" />
+      
       <div className='LeftSide'>
           <div className='PositionText'>
-            <div className='Title'>
-              Welcome Back
-            </div>
-            <div className='Subtitle'>
-              Welcome back! Please enter your details.
-            </div>
-            <br/>
+            <div className='Title'>Welcome Back</div>
+            <div className='Subtitle'>Welcome back! Please enter your details.</div><br/>
+
             <label className='Label'>Email</label><br/>
             <input className='Input' type="text" placeholder='Enter your email'></input><br/><br/>
             <label className='Label'>Password</label><br/>
-            <input className='Input' type="password" placeholder='Enter your password'></input>
+            <input className='Input' type="password" placeholder='Enter your password'></input><br/>
             
-            <br/>
-
             <input className='checkbox' type="checkbox"></input>
             <label className='Check'>Remember for 30 days</label>
 
-            <label className='ForgotPass'>Forgot password</label>
-            <br/>
-            <button className='BlackButton'>Sign In</button>
-            <br/>
-            <button className='GoogleButton'>Sign In With Google</button>
-            <br/>
+            <label className='ForgotPass'>Forgot password</label><br/>
+            
+            <button className='BlackButton'>Sign In</button><br/>
+            
+            <FcGoogle className='iconGoogle'/>
+            <button className='GoogleButton'>Sign In With Google</button><br/>
+            
             <label className='Last'>Dont't have an account?</label>
             <label className='SignUp'>Sign up for free</label>
-
 
           </div>
       </div>
 
-
       <div className='RightSide'>
           <img className='Capa' src="1.jpg" alt=""/>
       </div>
-
-    
     </div>
-
 
   );
 }
